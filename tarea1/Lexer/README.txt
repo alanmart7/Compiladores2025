01c4ec241ab2b298bb792bbd7f79440273a7efb9
alanmart7 <148172819+alanmart7@users.noreply.github.com>
2025-04-13 12:54:23 -0300

    Initial commit

---
+++ tarea1/Lexer/README.txt
@@ -0,0 +1,4 @@
+El siguiente codigo fue programado en JAVA Maven especialmente para el sistema operativo Windows.
+Para la correcta utilizacion del codigo es necesario crear la siguiente carpera en el disco local C "Analizador" (C:\Analizador).
+El archivo .json a utilizar debe estar almacenado en el directorio creado mas arriba. Asi tambien, el archivo .json debe llamarse fuente.json.
+Una vez realizado esto simplemente ejecutar el programa y el archivo de salida se generara en el mismo directorio.
---
+++ tarea1/Lexer/src/main/java/analizadorLexico/Token.java
@@ -0,0 +1,45 @@
+package analizadorLexico;
+
+public class Token {
+
+    private String value;
+    private Tipos tipo;
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public Tipos getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(Tipos tipo) {
+        this.tipo = tipo;
+    }
+
+    enum Tipos {
+        L_CORCHETE("\\["),
+        R_CORCHETE("\\]"),
+        L_LLAVE("\\{"),
+        R_LLAVE("\\}"),
+        COMA(","),
+        DOS_PUNTOS(":"),
+        LITERAL_CADENA("\"(?:\\\\.|[^\"\\\\])*\""),
+        LITERAL_NUM("-?[0-9]+(\\.[0-9]+)?([eE][+-]?[0-9]+)?"),
+        PR_TRUE("true|TRUE"),
+        PR_FALSE("false|FALSE"),
+        PR_NULL(null),
+        EOF("EOF");
+
+        public final String patron;
+
+        Tipos(String s) {
+            this.patron = s;
+        }
+    }
+
+}
